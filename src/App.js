8823b0a28b43614c3cdcdbf7eaddf177ab357568
Praful <praful@taglr.com>
2020-04-01 21:39:39 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import asyncComponent from './hoc/asynComponent/asyncComponent';
+
+class App extends React.Component {
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    return (
+      <div>
+        {routes}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
