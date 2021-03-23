059e90966b974d5006d18ea681ca36b14dc587ae
Praful <praful@taglr.com>
2021-03-23 15:13:33 +0530

    Fix error

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   
 });
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(rootReducer,
   applyMiddleware(thunk)
-));
+);
 
 const app = (
   <Provider store={store}>
